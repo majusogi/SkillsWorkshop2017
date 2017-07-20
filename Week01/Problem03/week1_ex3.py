ab2fbbcd3694db31c1da1a3d06989e897b52c274
awillats <awillats3@gatech.edu>
2017-07-20 15:51:25 -0400

    first 3 problems, in py2.7

---
+++ Week01/Problem03/week1_ex3.py
@@ -0,0 +1,51 @@
+#What is the largest prime factor of the number 600851475143 ?
+
+import math
+
+#prod = 13195;
+prod = 600851475143;
+
+rem = prod;
+
+
+isPrimeFlag = 0
+
+#if the return is 1, it's prime, otherwise return the factor you found
+
+#initially checked from 1 to num. Could stop at sqrt(num2check)?
+
+def isPrime(num2check):
+    i=2
+
+    while (i < math.sqrt(num2check)):
+        # for free whatever comes out of this has to be prime, since we search starting from 2.
+        # Non prime-factors have to be larger than their prime factors!
+        if math.fmod(num2check,i)==0:
+            return i
+        else:
+            i+=1
+
+    #if we got here without returning then it's prime
+    return 1
+#enddef
+
+
+# test cases for isPrime
+#print isPrime(7)
+#print isPrime(15)
+#print isPrime(prod)
+
+
+while isPrimeFlag==0:
+    prime_factor = isPrime(rem)
+
+    if prime_factor==1:
+        isPrimeFlag = 1
+        print "done:",rem
+    else:
+        print "remainder is ",rem,"*",prime_factor
+        rem = rem/prime_factor
+
+
+
+
